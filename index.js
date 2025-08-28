88efe477661c940845cc01823c43bc2838f6c0c5
vikash-mandal747 <vikashmandal747@gmail.com>
2025-08-28 10:03:51 +0530

    connecting FE and BE

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require("express");
+const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
-const app = express();
+const app = express();//I need express app but not express server, hence no app.listen
+//http server as well , since socket are upgraded http protocol
 const server = http.createServer(app);
+//http's server runs over express app, so that i can apply middleware in future
 
-const io = new Server(3000, server);//Socket.io's is created over http server
+const io = new Server(server, {
+    cors: {
+        origin: "http://127.0.0.1:5500",
+        method: ["GET", "POST"]
+    }
+});//Socket.io's is created over http server
 
-server.listen(8080,()=>{
+app.use(cors())
+
+//socket events here, io is the main chat server
+io.on("connection", (socket) => {
+    //socket means client
+    console.log("socket connected", socket.id);
+
+    socket.on("regName", (name) => {
+        console.log(name);
+    })
+
+    socket.on("sendMessage", (message) => {
+        console.log(message);
+    })
+
+})
+
+server.listen(9000, () => {
     console.log("server started");
 })

b203edff2b805e81b310e16fa7d5a543c3779930
vikash-mandal747 <vikashmandal747@gmail.com>
2025-11-06 23:19:47 +0530

    updated chatApp

--- index.js
+++ index.js
@@ -10,27 +10,38 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: "http://127.0.0.1:5500",
-        method: ["GET", "POST"]
+        methods: ["GET", "POST"]
     }
 });//Socket.io's is created over http server
 
 app.use(cors())
 
-//socket events here, io is the main chat server
+//socket events here, io is the main chat server 
+
+let userObj = {};
+let chatHistory = [];//chatobjets will be pushed
 io.on("connection", (socket) => {
     //socket means client
     console.log("socket connected", socket.id);
 
-    socket.on("regName", (name) => {
-        console.log(name);
+    socket.on("registerName", (name) => {
+        userObj[socket.id] = name
+        io.emit("chatHistory", chatHistory);//sends message to all sockets
+        //console.log(userObj);
     })
 
     socket.on("sendMessage", (message) => {
-        console.log(message);
+        //console.log(userObj[socket.id], message);
+        //push this chat message to chatHistory Array
+        let chat = { name: [userObj[socket.id]], message: message };
+        chatHistory.push(chat);
+        console.log(chatHistory);
+        //socket.emit("chatHistory", chatHistory);//this sends message to the same user not to everyone
+        io.emit("chatHistory", chatHistory);//sends message to all sockets
     })
 
 })
 
-server.listen(9000, () => {
-    console.log("server started");
+server.listen(3000, () => {
+    console.log("server started..");
 })
